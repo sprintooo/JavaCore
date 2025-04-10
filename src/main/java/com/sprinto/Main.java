740a1a62f108b85c224a8be10ac1af312d7a2d3b
sprintooo <himanshupatidar622@gmail.com>
2025-04-10 21:29:45 +0000

    learn: java arrays

--- src/main/java/com/sprinto/Main.java
+++ src/main/java/com/sprinto/Main.java
@@ -1,7 +1,6 @@
 package com.sprinto;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+    public static void main(String[] args){
     }
 }
